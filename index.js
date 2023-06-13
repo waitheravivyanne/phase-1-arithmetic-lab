1262300ede550990e7a675c212849efc6109dadc
vivian waithera <vivyannekori@gmail.com>
2023-06-14 01:35:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 =31;
+let num2=2;
+let multiply= num1*num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 =22;
+let num4 =9;
+let mod = num3 % num4;
+
+let max = Math.max(2,9,20,13,17);
